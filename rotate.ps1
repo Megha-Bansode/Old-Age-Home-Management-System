d1dd8180cec0996bdebddfcfec365c1d61111d3c
yashparaye64-cloud <yashparaye64@gmail.com>
2026-07-23 12:14:20 +0530

    Delete all contents

--- rotate.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile('C:\xampp\htdocs\old age home management\Old-Age-Home-Management-System\assets\images\traditional-pattern.png')
-$img.RotateFlip('Rotate270FlipNone')
-$img.Save('C:\xampp\htdocs\old age home management\Old-Age-Home-Management-System\assets\images\traditional-pattern-horiz.png', [System.Drawing.Imaging.ImageFormat]::Png)
-$img.Dispose()
